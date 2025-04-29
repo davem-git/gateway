2e1539a8d96725cfe0355f4e0bd287fd89436335
davem-git <demathieu@gmail.com>
2025-04-29 13:14:13 -0500

    deepcopy.go fix

--- internal/ir/xds.go
+++ internal/ir/xds.go
@@ -2325,11 +2325,13 @@ type TCPKeepalive struct {
 	Interval *uint32 `json:"interval,omitempty" yaml:"interval,omitempty"`
 }
 
+// 
+// +k8s:deepcopy-gen=true
 type NetworkFilter struct {
     Name   string      `json:"name" yaml:"name"`
     Config *RBACConfig `json:"config,omitempty" yaml:"config,omitempty"`
 }
-
+// +k8s:deepcopy-gen=true
 type RBACConfig struct {
     Rules              []*AuthorizationRule       `json:"rules,omitempty" yaml:"rules,omitempty"`
     DefaultAction      egv1a1.AuthorizationAction `json:"defaultAction,omitempty" yaml:"defaultAction,omitempty"`
@@ -2337,6 +2339,7 @@ type RBACConfig struct {
     SourceIPEnforcement bool                      `json:"sourceIPEnforcement,omitempty" yaml:"sourceIPEnforcement,omitempty"`
 }
 
+// +k8s:deepcopy-gen=true
 type AuthorizationAction string
 
 const (
